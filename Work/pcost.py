a887843cfd7ab3b91e8df2e26619ad85612a0108
Dash Nash <dashnash@amazon.com>
2020-06-19 21:50:55 -0700

    Finishing Lesson 2.2

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,27 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+import sys
+
+def portfolio_cost(filename):
+    cost = 0
+    with open(filename) as f:
+        rows = csv.reader(f)
+        next(rows)
+        for row in rows:
+            try:
+                cost += float(row[1]) * float(row[2])
+            except ValueError as vE:
+                print('Error in Line:', row)
+                print(vE)
+    
+    return cost
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost('Data/missing.csv')
+print('Total Cost: ' + str(cost))
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,59 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+import sys
+
+def read_portfolio(filename):
+    portfolio = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        next(rows)
+        for row in rows:
+            try:
+                portfolio.append(
+                    {
+                        'name': row[0],
+                        'shares': int(row[1]),
+                        'value': float(row[2])
+                    })
+            except ValueError as vE:
+                print('Error in Line:', row)
+                print(vE)
+    
+    return portfolio
+
+def read_prices(filename):
+    'reads set of stock prices from filename and returns a dictionary'
+    prices = {}
+    with open(filename) as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                name, price = row
+                prices[name] = float(price)
+            except ValueError as vE:
+                print('Error:', row)
+                print(vE)
+
+    return prices
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+portfolio = read_portfolio(filename)
+prices = read_prices('Data/prices.csv')
+
+curr_value = 0.0
+acq_value = 0.0
+for holding in portfolio:
+    acq_value += holding['shares'] * holding['value']
+    curr_value += prices[holding['name']] * holding['shares']
+
+print('Current Value:', curr_value)
+
+change = curr_value - acq_value
+
+print('Gain/Loss:', change)
