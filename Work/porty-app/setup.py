440778fd9d48745d931c53719ec4041f8b796fec
Dash Nash <dashnash@amazon.com>
2020-06-23 16:11:48 -0700

    END: packaging and distribution

---
+++ Work/porty-app/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="porty",
+    version="0.0.1",
+    author="Dash",
+    author_email="dash@example.com",
+    description="Practical Python Code",
+    packages=setuptools.find_packages(),
+)
