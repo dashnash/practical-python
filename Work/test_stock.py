65113298d34ec3d5d20c68ff8e1a1fcb16fd34d8
Dash Nash <dashnash@amazon.com>
2020-06-23 15:35:25 -0700

    8.1 - Unit test

---
+++ Work/test_stock.py
@@ -0,0 +1,32 @@
+#test_stock.py
+
+import unittest
+import stock
+
+class TestStock(unittest.TestCase):
+    
+    
+    def test_create(self):
+        s = stock.Stock('GOOG', 100, 490.1)
+        self.assertEqual(s.name, 'GOOG')
+        self.assertEqual(s.shares, 100)
+        self.assertEqual(s.price, 490.1)
+
+    def test_cost(self):
+        s = stock.Stock('GOOG', 100, 490.1)
+        self.assertEqual(s.cost, 100 * 490.1)
+
+    def test_sell(self):
+        s = stock.Stock('GOOG', 100, 490.1)
+        s.sell(25)
+        self.assertEqual(s.shares, 100-25)
+
+    def test_bad_shares(self):
+        with self.assertRaises(TypeError):
+            s = stock.Stock('GOOG', 'many', 490.1)
+        s = stock.Stock('GOOG', 100, 490.1)
+        with self.assertRaises(TypeError):
+            s.shares = 'many'
+
+if __name__ == '__main__':
+    unittest.main()
